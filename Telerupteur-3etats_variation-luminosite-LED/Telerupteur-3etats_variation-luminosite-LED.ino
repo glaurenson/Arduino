4418b16b4641d64d10e11b838343857b7cbfc50a
Gary Laurenson <gary@laurenson.fr>
2015-02-07 17:40:47 +0100

    Modification
    Prise en compte systématique de l’état du bouton poussoir.

--- Telerupteur-3etats_variation-luminosite-LED/Telerupteur-3etats_variation-luminosite-LED.ino
+++ Telerupteur-3etats_variation-luminosite-LED/Telerupteur-3etats_variation-luminosite-LED.ino
@@ -8,7 +8,6 @@ int bpState;
 int oldBpState;
 int lastState = 1;
 int i = 0;
-boolean done = false;
 
 void setup() {
   pinMode(BP, INPUT);
@@ -18,7 +17,6 @@ void setup() {
 void loop () {
   digitalWrite(BP , HIGH);
   bpState = digitalRead(BP);
-  delay (100);
   
   if(bpState != oldBpState) {
     oldBpState = !bpState;
@@ -28,37 +26,25 @@ void loop () {
       if(lastState > 3) {
         lastState = 1;
       }
-      
-      switch(lastState) {
-        case 1 :
-          if(!done) {
-            digitalWrite(LED, LOW);
-          } else {
-            for(i = 255; i>= 0; i-=5) {
-              analogWrite(LED, i);
-              delay(3);
-            }
-          }
-          
-          break;
-          
-        case 2 :
-          for(i = 0; i<= 40; i++) {
-            analogWrite(LED, i);
-            delay(15);
-          }
-          break;
-          
-        case 3 :
-          for(i = 40; i<= 255; i++) {
-            analogWrite(LED, i);
-            delay(3);
-          }
-          done = true;
-          break;
-      }
+      oldBpState = bpState;
     }
-
-    oldBpState = bpState;
   }
+
+  switch(lastState) {
+    case 1 :
+      if(i>0) i--;
+      break;
+      
+    case 2 :
+      if(i < 40) i++;
+      if(i > 40) i--;
+      break;
+      
+    case 3 :
+      if(i < 255) i++;
+      break;
+  }
+
+  analogWrite(LED, i);
+  delay(5);
 }
