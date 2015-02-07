fe09c4db3f274adb5674ed1cb1e4afb574504254
Gary Laurenson <gary@laurenson.fr>
2015-02-07 12:49:28 +0100

    Ajout de la variation (effet "Fade")

--- Telerupteur-3etats_variation-luminosite-LED/Telerupteur-3etats_variation-luminosite-LED.ino
+++ Telerupteur-3etats_variation-luminosite-LED/Telerupteur-3etats_variation-luminosite-LED.ino
@@ -4,6 +4,8 @@ const int LED = 5;
 int bpState;
 int oldBpState;
 int lastState = 1;
+int i = 0;
+boolean done = false;
 
 void setup() {
   pinMode(BP, INPUT);
@@ -19,25 +21,41 @@ void loop () {
     oldBpState = !bpState;
     if(!bpState) {
       lastState++;
+      
+      if(lastState > 3) {
+        lastState = 1;
+      }
+      
+      switch(lastState) {
+        case 1 :
+          if(!done) {
+            digitalWrite(LED, LOW);
+          } else {
+            for(i = 255; i>= 0; i-=5) {
+              analogWrite(LED, i);
+              delay(3);
+            }
+          }
+          
+          break;
+          
+        case 2 :
+          for(i = 0; i<= 40; i++) {
+            analogWrite(LED, i);
+            delay(15);
+          }
+          break;
+          
+        case 3 :
+          for(i = 40; i<= 255; i++) {
+            analogWrite(LED, i);
+            delay(3);
+          }
+          done = true;
+          break;
+      }
     }
-    
-    if(lastState > 3) {
-      lastState = 1;
-    }
+
     oldBpState = bpState;
-    
-    switch(lastState) {
-      case 1 :
-        digitalWrite(LED, LOW);
-        break;
-        
-      case 2 :
-        analogWrite(LED, 40);
-        break;
-        
-      case 3 :
-        digitalWrite(LED, HIGH);
-        break;
-    }
   }
 }
